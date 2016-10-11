907ac01597b9688bb1b1ffcec3252569303dd529
neuroradiology <github.neuroradiology@spinsnout.com>
2016-10-11 19:10:21 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Arc.Ecto.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:arc,  "~> 0.5.3"},
+      {:arc,  "~> 0.6.0-rc1"},
       {:ecto, "~> 2.0"},
       {:mock, "~> 0.1.1", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev}
