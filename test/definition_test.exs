a30a3dbb436524390f80dc0b1d165b7e3443b4ad
Sean Stavropoulos <sean.stavro@gmail.com>
2017-04-20 12:47:52 -0700

    fix test

--- test/definition_test.exs
+++ test/definition_test.exs
@@ -5,7 +5,8 @@ defmodule ArcTest.Ecto.Definition do
     def url(_, :original, _), do: "fallback"
     def url(_, :signed, _), do: "fallback?a=1&b=2"
     def store({file, _}), do: {:ok, file}
-    defoverridable [url: 3]
+    def delete(_), do: :ok
+    defoverridable [delete: 1, url: 3]
     use Arc.Ecto.Definition
   end
 
