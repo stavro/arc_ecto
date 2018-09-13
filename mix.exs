5613632a9a1307722278ddc521d6b6630987cace
Maarten Jacobs <m.jacobs@defacto.nl>
2018-09-13 22:09:31 +0200

    update to latest Ecto

--- mix.exs
+++ mix.exs
@@ -4,15 +4,17 @@ defmodule Arc.Ecto.Mixfile do
   @version "0.11.0"
 
   def project do
-    [app: :arc_ecto,
-     version: @version,
-     elixir: "~> 1.4",
-     elixirc_paths: elixirc_paths(Mix.env),
-     deps: deps(),
+    [
+      app: :arc_ecto,
+      version: @version,
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      deps: deps(),
 
-    # Hex
-     description: description(),
-     package: package()]
+      # Hex
+      description: description(),
+      package: package()
+    ]
   end
 
   # Configuration for the OTP application
@@ -24,7 +26,7 @@ defmodule Arc.Ecto.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp description do
     """
@@ -33,16 +35,18 @@ defmodule Arc.Ecto.Mixfile do
   end
 
   defp package do
-    [maintainers: ["Sean Stavropoulos"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/stavro/arc_ecto"},
-     files: ~w(mix.exs README.md lib)]
+    [
+      maintainers: ["Sean Stavropoulos"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/stavro/arc_ecto"},
+      files: ~w(mix.exs README.md lib)
+    ]
   end
 
   defp deps do
     [
-      {:arc,  "~> 0.11.0"},
-      {:ecto, "~> 2.1"},
+      {:arc, "~> 0.11.0"},
+      {:ecto, "~> 3.0.0-rc.1"},
       {:mock, "~> 0.1.1", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
--- test/definition_test.exs
+++ test/definition_test.exs
@@ -2,7 +2,7 @@ defmodule ArcTest.Ecto.Definition do
   use ExUnit.Case
 
   test "defines Definition.Type module" do
-    assert DummyDefinitionTwo.Type.type == Arc.Ecto.Type.type
+    assert DummyDefinitionTwo.Type.type() == Arc.Ecto.Type.type()
   end
 
   test "falls back to pre-defined url" do
@@ -11,18 +11,34 @@ defmodule ArcTest.Ecto.Definition do
 
   test "url appends timestamp to url with no query parameters" do
     updated_at = NaiveDateTime.from_erl!({{2015, 1, 1}, {1, 1, 1}})
-    url = DummyDefinitionTwo.url({%{file_name: "test.png", updated_at: updated_at}, :scope}, :original, [])
+
+    url =
+      DummyDefinitionTwo.url(
+        {%{file_name: "test.png", updated_at: updated_at}, :scope},
+        :original,
+        []
+      )
+
     assert url == "fallback?v=63587293261"
   end
 
   test "url appends timestamp to url with query parameters" do
     updated_at = NaiveDateTime.from_erl!({{2015, 1, 1}, {1, 1, 1}})
-    url = DummyDefinitionTwo.url({%{file_name: "test.png", updated_at: updated_at}, :scope}, :signed, [])
+
+    url =
+      DummyDefinitionTwo.url(
+        {%{file_name: "test.png", updated_at: updated_at}, :scope},
+        :signed,
+        []
+      )
+
     assert url == "fallback?a=1&b=2&v=63587293261"
   end
 
   test "url is unchanged if no timestamp is present" do
-    url = DummyDefinitionTwo.url({%{file_name: "test.png", updated_at: nil}, :scope}, :original, [])
+    url =
+      DummyDefinitionTwo.url({%{file_name: "test.png", updated_at: nil}, :scope}, :original, [])
+
     assert url == "fallback"
   end
 end
